9baf62ec7faadc9465d71ec474deda5f38e8933c
sableRaf <groupes.raphael@gmail.com>
2017-02-18 18:39:19 +0100

    changed code example

--- login.js
+++ login.js
@@ -1,51 +1,50 @@
-// This is called with the results from from FB.getLoginStatus().
-function statusChangeCallback(response) {
 
-	// The response object is returned with a status field that lets the app 	know 		the current login status of the person.
-  if (response.status === 'connected') {
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function(response) {
-      console.log('Successful login for: ' + response.name);
-      document.getElementById('status').innerHTML =
-        'Thanks for logging in, ' + response.name + '!';
-    });
-  } else if (response.status === 'not_authorized') {
-    // The person is logged into Facebook, but not your app.
-    document.getElementById('status').innerHTML = 'Please log ' + 'into this app.';
-  } else {
-    // The person is not logged into Facebook, so we're not sure if they are logged into this app or not.
-    document.getElementById('status').innerHTML = 'Please log ' + 'into Facebook.';
-  }
+// Source is this tutorial
+// http://www.devils-heaven.com/facebook-javascript-sdk-login/
 
-}
-
-// This function is called when someone finishes with the Login Button.
-function checkLoginState() {
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-  });
+function getUserData() {
+	FB.api('/me', function(response) {
+		document.getElementById('response').innerHTML = 'Hello ' + response.name;
+	});
 }
 
 window.fbAsyncInit = function() {
-  FB.init({
-    appId : '{your-app-id}',
-    cookie : true, // enable cookies to allow the server to access the session
-    xfbml : true, // parse social plugins on this page
-    version : 'v2.1' // use version 2.1
-  });
+	//SDK loaded, initialize it
+	FB.init({
+		appId      : 'your-app-id',
+		xfbml      : true,
+		version    : 'v2.2'
+	});
 
-  // Now that we've initialized the JavaScript SDK, we call FB.getLoginStatus(). 
-  // This function gets the state of the person visiting this page.
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-  });
+	//check user session and refresh it
+	FB.getLoginStatus(function(response) {
+		if (response.status === 'connected') {
+			//user is authorized
+			document.getElementById('loginBtn').style.display = 'none';
+			getUserData();
+		} else {
+			//user is not authorized
+		}
+	});
 };
 
-// Load the SDK asynchronously
-(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
+//load the JavaScript SDK
+(function(d, s, id){
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) {return;}
+	js = d.createElement(s); js.id = id;
+	js.src = "//connect.facebook.com/en_US/sdk.js";
+	fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
+
+//add event listener to login button
+document.getElementById('loginBtn').addEventListener('click', function() {
+	//do the login
+	FB.login(function(response) {
+		if (response.authResponse) {
+			//user just authorized your app
+			document.getElementById('loginBtn').style.display = 'none';
+			getUserData();
+		}
+	}, {scope: 'email,public_profile', return_scopes: true});
+}, false);
